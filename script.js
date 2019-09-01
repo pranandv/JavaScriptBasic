ac6b06bf8be51134a46570e7ec8118257a8e741f
pranand <pranandv@gmail.com>
2019-09-01 11:15:19 +0530

    final output ready

---
+++ script.js
@@ -0,0 +1,12 @@
+const colorbtn=document.querySelector('.colorbtn');
+const colorBcg=document.querySelector('body');
+
+const colors=['yellow','red','green','blue' ];
+
+colorbtn.addEventListener('click',changeColor);
+
+function changeColor(){
+    let random=Math.floor(Math.random()*colors.length)
+
+    colorBcg.style.backgroundColor = colors[random];
+}0
